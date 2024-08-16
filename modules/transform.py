7693c00c8a770f398314273757c862f322713f6e
Aligehri <ghazzael5h@gmail.com>
2024-08-15 22:25:31 -0600

    Chequeo de data nulo

--- modules/transform.py
+++ modules/transform.py
@@ -11,6 +11,16 @@ def read_json(file):
         print(f'El archivo {file} no se encuentra.')
         return None
 
+#Need to check key by key because there might not be a dictionary within another dictionary:
+
+def check_data(item, keys):
+    value = item
+    for key in keys:
+        if value is not None and key in value:
+            value = value[key]
+        else:
+            return "No data"
+    return value
 
 def transform_data():
 
@@ -30,7 +40,8 @@ def transform_data():
         'Release date':[],
         'Track popularity':[],
         'Track id':[],
-        'Artist id':[]
+        'Artist id':[],
+        'Date loaded data':[]
         }
 
     top_tracks_df = {
@@ -40,7 +51,8 @@ def transform_data():
         'Duration h:m:s':[], 
         'Release date':[], 
         'Popularity':[], 
-        'Track id':[] 
+        'Track id':[],
+        'Date loaded data':[]
         }
 
     top_artists_df = {
@@ -48,45 +60,85 @@ def transform_data():
         'Followers':[],
         'Genres':[],
         'Popularity':[],
-        'Artist id':[]
+        'Artist id':[],
+        'Date loaded data':[]
     }
 
     #Append data:
 
     for item in recently['items']:
-        recently_df['Track name'].append(item['track']['name'])
-        recently_df['Artist'].append(item['track']['artists'][0]['name'])
-        recently_df['Album'].append(item['track']['album']['name'])
-        recently_df['Duration h:m:s'].append( str( datetime.timedelta(milliseconds=item['track']['duration_ms'])).split('.')[0])
-        recently_df['Day played'].append(item['played_at'][0:10])
-        recently_df['Time played'].append( item['played_at'][11:22] )
-        recently_df['Release date'].append( item['track']['album']['release_date'] )
-        recently_df['Track popularity'].append (item['track']['popularity'])
-        recently_df['Track id'].append( item['track']['id'] )
-        recently_df['Artist id'].append( item['track']['artists'][0]['id'] )
+        recently_df['Track name'].append(check_data(item, ['track', 'name']))
 
-    save_jason('recently_transformed.json',recently_df)
+        artist_check = check_data(item, ['track','artists'])
+        if artist_check != 'No data':
+            recently_df['Artist'].append(check_data(artist_check[0], ['name'] ))
+            recently_df['Artist id'].append(check_data(artist_check[0],['id']))
+        else:
+            recently_df['Artist'].append('No data')
+            recently_df['Artist id'].append('No data')
+
+        recently_df['Album'].append(check_data(item, ['track', 'album', 'name']))
+        
+        duration_ms = check_data(item, ['track', 'duration_ms'])
+        if duration_ms != 'No data':
+            duration = str(datetime.timedelta(milliseconds=duration_ms)).split('.')[0]
+            recently_df['Duration h:m:s'].append(duration)
+        else:
+            recently_df['Duration h:m:s'].append('No data')
+
+        recently_df['Day played'].append(check_data(item, ['played_at'])[0:10])
+        recently_df['Time played'].append(check_data(item, ['played_at'])[11:22])
+        recently_df['Release date'].append(check_data(item, ['track', 'album', 'release_date']))
+        recently_df['Track popularity'].append(check_data(item, ['track', 'popularity']))
+        recently_df['Track id'].append(check_data(item, ['track', 'id']))
+        recently_df['Date loaded data'].append(str(datetime.date.today()))
+
+    
 
     for item in top_tracks['items']:
-        top_tracks_df['Track name'].append(item['name'])
-        top_tracks_df['Artist'].append(item['artists'][0]['name'])
-        top_tracks_df['Album'].append(item['album']['name'])
-        top_tracks_df['Duration h:m:s'].append( str( datetime.timedelta(milliseconds=item['duration_ms'])).split('.')[0])
-        top_tracks_df['Release date'].append(item['album']['release_date'])
-        top_tracks_df['Popularity'].append(item['popularity'])
-        top_tracks_df['Track id'].append(item['id'])
+        top_tracks_df['Track name'].append(check_data(item,['name']))
 
-    save_jason('top_tracks_transformed.json',top_tracks_df)
+        artist_check = check_data(item, ['artists'])
+        if artist_check != 'No data':
+            top_tracks_df['Artist'].append(check_data(artist_check[0],['name']))
+        else:
+            recently_df['Artist'].append('No data')
+        
+        top_tracks_df['Album'].append(check_data(item,['album','name']))
+
+        duration_ms = check_data(item, ['duration_ms'])
+        if duration_ms != 'No data':
+            duration = str( datetime.timedelta(milliseconds=duration_ms)).split('.')[0]
+            top_tracks_df['Duration h:m:s'].append(duration)
+        else:
+            top_tracks_df['Duration h:m:s'].append('No data')
+        
+        top_tracks_df['Release date'].append(check_data(item,['album', 'release_date']))
+        top_tracks_df['Popularity'].append(check_data(item,['popularity']))
+        top_tracks_df['Track id'].append(check_data(item,['id']))
+        top_tracks_df['Date loaded data'].append(str(datetime.date.today()))
+
+    
 
     for item in top_artists['items']:
-        top_artists_df['Artist'].append(item['name'])
-        top_artists_df['Followers'].append(item['followers']['total'])
-        top_artists_df['Genres'].append(', '.join(item['genres']))
-        top_artists_df['Popularity'].append(item['popularity'])
-        top_artists_df['Artist id'].append(item['id'])
+        top_artists_df['Artist'].append(check_data(item,['name']))
+        top_artists_df['Followers'].append(check_data(item,['followers', 'total']))
+        
+        genres_check = check_data(item,['genres'])
+        if genres_check != 'No data':
+            top_artists_df['Genres'].append(', '.join(genres_check))
+        else:
+            top_artists_df['Genres'].append('No data')
+        
+        top_artists_df['Popularity'].append(check_data(item,['popularity']))
+        top_artists_df['Artist id'].append(check_data(item,['id']))
+        top_artists_df['Date loaded data'].append(str(datetime.date.today()))
 
+    
+
+    save_jason('recently_transformed.json',recently_df)
+    save_jason('top_tracks_transformed.json',top_tracks_df)
     save_jason('top_artists_transformed.json', top_artists_df)
-
 
     recently_df = pd.DataFrame(recently_df)
     top_tracks_df = pd.DataFrame(top_tracks_df)
